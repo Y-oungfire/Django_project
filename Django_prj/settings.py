ef598678eb61dff432e189df4fcefcccb0fe6b08
lee <lweyine@naver.com>
2022-06-18 02:00:13 +0900

    댓글 생성

--- Django_prj/settings.py
+++ Django_prj/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'crispy_forms',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 
     'community',
 ]
@@ -102,6 +107,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # Internationalization
--- community/admin.py
+++ community/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from community.models import Post, Category
+from community.models import Post, Category, Comment
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
