619bb9998c9d9e9226fdf52732d28c2d831c689b
lee <lweyine@naver.com>
2022-06-17 15:39:25 +0900

    첨부파일 추가

--- community/models.py
+++ community/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 
 # Create your models here.
@@ -8,7 +10,7 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to='community/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='community/files/%Y/%m/%d', blank=True)
-    
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -19,3 +21,9 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/community/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
