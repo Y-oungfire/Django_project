aa77369a0cf18230d52fb2917d8fd6fbac9042de
lee <lweyine@naver.com>
2022-06-17 20:55:57 +0900

    카테고리 생성

--- community/models.py
+++ community/models.py
@@ -5,6 +5,20 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/community/category/{self.slug}'
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -16,6 +30,8 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
--- community/views.py
+++ community/views.py
@@ -1,25 +1,74 @@
+
+
 from django.shortcuts import render
-from .models import Post
+from django.views.generic import ListView, DetailView
+from .models import Post, Category
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+
+        return context
+
+class PostDetail(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+
+        return context
+
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     category = Category.objects.all()
+#
+#
+#     return render(
+#         request,
+#         'community/post_list.html',
+#         {
+#             'posts' : posts,
+#             'categories' : category,
+#         }
+#     )
+
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     category = Category.objects.all()
+#
+#     return render(
+#         request,
+#         'community/post_detail.html',
+#         {
+#             'post' : post,
+#             'categories': category,
+#         }
+#     )
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
 
     return render(
         request,
         'community/post_list.html',
         {
-            'posts' : posts,
-        }
-    )
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'community/post_detail.html',
-        {
-            'post' : post,
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=category).count(),
+            'category': category,
         }
     )
