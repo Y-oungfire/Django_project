ecf740db6b2f3f03f9b60147b9ff3c0af226a741
Y-ongfire <lweyine.sm@gmail.com>
2022-06-19 11:13:01 +0900

    ui 수정

--- community/views.py
+++ community/views.py
@@ -56,10 +56,10 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
 def category_page(request, slug):
     if slug == 'no_category':
         category = '미분류'
-        post_list = Post.objects.filter(category=None)
+        post_list = Post.objects.filter(category=None).order_by('-pk')
     else:
         category = Category.objects.get(slug=slug)
-        post_list = Post.objects.filter(category=category)
+        post_list = Post.objects.filter(category=category).order_by('-pk')
 
     return render(
         request,
@@ -98,6 +98,15 @@ class CommentUpdate(LoginRequiredMixin, UpdateView):
             return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
 
 
 
