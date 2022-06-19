badc74acf7bb8314c0aed15f35adffc557109758
lee <lweyine@naver.com>
2022-06-20 02:56:46 +0900

    ui 수정

--- community/forms.py
+++ community/forms.py
@@ -2,9 +2,7 @@ from .models import Comment
 from django import forms
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(label='Content', max_length=200, widget=forms.Textarea(attrs={'rows': 5, 'cols': 5}))
     class Meta:
         model = Comment
         fields = ('content',)
-        widgets = {
-            'message' : forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-        }
